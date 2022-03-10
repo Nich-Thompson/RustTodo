08938bf37bd46ba4edf92cf2447d67258fa53d22
Jesse <nicholasthompsonlovesbeans@gmail.com>
2022-03-10 13:03:51 +0100

    step 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("Please select an action");
+    let item = std::env::args().nth(2).expect("Please select an item");
+    
+    println!("{:?}, {:?}", action, item);
 }
