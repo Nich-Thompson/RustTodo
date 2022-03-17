14395e158713a78acc956529b1aaca9cac82b10a
Jesse <nicholasthompsonlovesbeans@gmail.com>
2022-03-17 18:39:02 +0100

    alternate syntax

--- dist/index-dc714151864ebac9.js
+++ dist/index-dc714151864ebac9.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-c6d5b10dd73d1578_bg.wasm', import.meta.url);
+        input = new URL('index-dc714151864ebac9_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -474,8 +474,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper2520 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 92, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper2419 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 91, __wbg_adapter_18);
         return addHeapObject(ret);
     };
 
--- src/main.rs
+++ src/main.rs
@@ -20,42 +20,27 @@ fn app() -> Html {
 
     let state = use_state(|| Todo {
         // content: Vec::new()
-        content: vec!["wow1".to_owned(), "wow2".to_owned()]
+        content: vec!["Your".to_owned(), "Todos".to_owned(), "Here".to_owned()]
     });
 
-    // let state2 = state.clone(); // this dont work
-    // let mut old_content = state2.content.clone();
-    // old_content.push("wow1".to_owned());
-    // old_content.push("wow2".to_owned());
-    // old_content.push("wow3".to_owned());
-
     let onclick = {
-        // todo.content.push("wow".to_owned());
-        // todo.save();
-        // let mut queue: () = state.content.as_mut().unwrap();
-
         let state = state.clone();
-        let mut oldContent = state.content.clone();
-        oldContent.push("wow".to_owned());
+        let mut old_content = state.content.clone();
+        old_content.push("wow".to_owned());
 
         Callback::from(move |_| {
             state.set(Todo {
-                content: oldContent.clone()
+                content: old_content.clone()
             })
         })
     };
 
-    let items = (1..=10).collect::<Vec<_>>();
-
     html! {
         <div>
             <button {onclick}>{ "Click me!" }</button>
-            // <span>{ todo.map[""|] }</span>
             <ul class="item-list">
-                { state.content.iter().collect::<Html>() }
-            </ul>
-            <ul class="item-list">
-                { items.iter().collect::<Html>() }
+                // { state.content.iter().collect::<Html>() }
+                { for state.content.iter() }
             </ul>
         </div>
     }
