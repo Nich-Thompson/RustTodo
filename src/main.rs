ba41aaa84012d9259986f4f6beb42f3fabce5d3a
Jesse <nicholasthompsonlovesbeans@gmail.com>
2022-03-17 14:36:00 +0100

    failure
    does not compile
    thats what you get for following a 4y old tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,55 @@
+#[macro_use]
+extern crate yew;
+
+use yew::html::*;
+
+struct Model {
+    input: String,
+    todos: Vec<String>,
+}
+
+enum Msg {
+    Add,
+    Update(String),
+    Remove(usize),
+    RemoveAll,
+    Nothing
+}
+
+fn update(_: &mut Context<Msg>, model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::Add => {}
+        Msg::Update(s) => {}
+        Msg::Remove(i) => {}
+        Msg::RemoveAll => {}
+        Msg::Nothing => {}
+    }
+}
+
+fn view(model: &Model) -> Html<Msg> {
+    html! {
+        <div>
+            <div>
+                <h1>{"Todo App"}</h1>
+                <input />
+            </div>
+            <div>
+                <button>{"Delete all todos"}</button>
+            </div>
+            <div>
+                <ul>
+                </ul>
+            </div>
+        </div>
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let model = Model {
+        todos: vec![],
+        input: "".to_string(),
+    };
+    program(model, update, view);
 }
 
