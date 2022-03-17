3817fee8f5bdee807bba62f0f68ee5f318df5a6f
Jesse <nicholasthompsonlovesbeans@gmail.com>
2022-03-17 14:05:55 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::response::content::Html;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +7,34 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/")]
+fn index2() -> &'static str {
+    "Hello, world! 2"
+}
+
+#[get("/")]
+fn index3() -> Html<&'static str> {
+    Html(r#"
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>My App</title>
+                <link data-trunk rel="css" href="style.css">
+            </head>
+            <body>
+                <div>
+                    <button>Click me!</button>
+                </div>
+            </body>
+        </html>
+    "#)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/2", routes![index2])
+    .mount("/3", routes![index3])
 }
