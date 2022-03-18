6ddeb97fb27c45f1ae9cee7f47b78985eeb098f3
Jesse <nicholasthompsonlovesbeans@gmail.com>
2022-03-18 13:03:48 +0100

    giving up
    closest thing:
    https://yew.rs/docs/0.18.0/concepts/html/events#oninput-and-onchange

--- dist/index-4fdd50f0ac78320f.js
+++ dist/index-4fdd50f0ac78320f.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b9dfe21187b632df_bg.wasm', import.meta.url);
+        input = new URL('index-4fdd50f0ac78320f_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -474,7 +474,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper2448 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2449 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 91, __wbg_adapter_18);
         return addHeapObject(ret);
     };
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,44 @@
 use yew::prelude::*;
 
 struct Todo {
-    content: Vec<String>
+    content: Vec<String>,
+    input: String
 }
 
 #[function_component(App)]
 fn app() -> Html {
     let state = use_state(|| Todo {
-        content: vec!["Your".to_owned(), "Todos".to_owned(), "Here".to_owned()]
+        content: vec!["Your".to_owned(), "Todos".to_owned(), "Here".to_owned()],
+        input: String::from("Test")
     });
 
-    let mut btn_val = "Test";
+    // let mut btn_val = "Test";
 
     let onclick = {
         let state = state.clone();
         let mut old_content = state.content.clone();
         // old_content.push("wow".to_owned());
-        old_content.push(btn_val.to_owned());
-        btn_val = "wow";
+        old_content.push(state.input.clone());
+
+        // old_content.remove(0); // test
+        // old_content.push(btn_val.to_owned());
+        // state.input = "wow".to_string();
 
         Callback::from(move |_| {
             state.set(Todo {
-                content: old_content.clone()
+                content: old_content.clone(),
+                input: state.input.clone()
             })
         })
     };
 
     html! {
         <div>
-            <input type="text" value={ btn_val }/>
+            // <input type="text" value={ btn_val }/>
+            <input type="text" 
+                value={ state.input.to_string() }
+                // oninput={ oninput }
+            />
             <button { onclick }>{ "Click me!" }</button>
             <ul class="item-list">
                 {
